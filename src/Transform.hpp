b8dc72a987a87db73274b0b1e6c7d544c01c420a
Joshua Tully <joshuartully@gmail.com>
2015-12-08 17:29:33 -0600

    Started work on mat4 and transform classes.

---
+++ src/Transform.hpp
@@ -0,0 +1,14 @@
+#ifndef TRANSFORM_HPP
+#define TRANSFORM_HPP
+
+#pragma once
+#include "mat4.hpp"
+
+class Transform {
+public:
+
+private:
+	mat4 m, minv;
+};
+
+#endif //TRANSFORM_HPP
---
+++ src/mat4.hpp
@@ -0,0 +1,11 @@
+#ifndef MAT4_HPP
+#define MAT4_HPP
+
+#pragma once
+
+
+class mat4 {
+public:
+	float d[16];
+};
+#endif // !MAT4_HPP
